832955d09b2ac5a1e08ce0d2f45203c2428f7ec3
dang0x5f <dannypgehrz@gmail.com>
2024-06-05 06:48:50 -0500

    Updating README, Renaming Repo for clarity, and minor changes.

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
-# swixm
+# pomobar (pomodoro on xmobar)
 
-lightweight pomodoro study timer
+Pomodoro study timer that's designed specifically with xmobar in mind.
+Requires [herbe](https://github.com/dudik/herbe) for notifications and warnings.
+Requires some type of script to handle output and ability to kill process with PID.
 
-designed for my specific xmobar setup, but is to be set at the command line.
+## Usage
 
-requires herbe for gui notifications
+pomo mins
+
+`user@host:~ $ pomo 30`
+runs a timer for 30 minutes
---
+++ pomo.c
@@ -0,0 +1,188 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <string.h>
+#include <sys/stat.h>
+
+#define RUNNING 1
+#define TITLE "POMODORO"
+#define LOCKFILE "/tmp/pomo.lockfile"
+
+typedef enum { START, WARNING, TERMINATE, END } ForkCase_t;
+
+void itoa(int, char *);
+void timetoa(int, char *);
+void reverse(char *);
+void sig_handler(int);
+void fork_failed(void);
+void fork_handler(ForkCase_t);
+void write_to_lockfile(int, char *, char *, char *);
+
+int main(int argc, char *argv[]){
+    /* TODO: add 5 minute break */
+    /* TODO: + current_time buffer to herbe message */
+    int seconds, quarter, fd;
+    double difference;
+    time_t now, future;
+    char delim = ',';
+    char pid_str[10];
+    char current_time[10]; 
+    char quarter_time[10];
+
+    if(argc < 2 || (seconds = 60 * atoi(argv[1])) <= 0){
+        fprintf(stderr, "usage: %s mins \n", argv[0]);
+        exit(1);
+    }
+    if((fd = open(LOCKFILE, O_WRONLY|O_CREAT|O_EXCL, S_IRWXU)) == -1){
+        fprintf(stderr, "lock file exists. one instance may already be running \n");
+        exit(1);
+    }
+    else{
+        time(&now);
+        future = now + seconds;
+        difference = difftime(future, now);  
+
+        itoa(getpid(), pid_str);
+        timetoa(difference, current_time);
+
+        write_to_lockfile(fd, pid_str, &delim, current_time);
+
+        close(fd);
+    }
+
+    signal(SIGTERM, sig_handler);
+    signal(SIGINT, sig_handler);
+    quarter = seconds / 4;
+    timetoa(quarter, quarter_time);
+
+    fork_handler(START);
+    while(RUNNING){
+        sleep(1);
+        now += 1;
+
+        difference = difftime(future, now);  
+        timetoa(difference, current_time);
+        /* printf("%.0f \n", difference); */
+        /* printf("%s \n", current_time); */
+        if((fd = open(LOCKFILE, O_WRONLY|O_TRUNC)) == -1){
+            fprintf(stderr, "lock file exists. one instance may already be running \n");
+            exit(1);
+        }
+        else{
+            write_to_lockfile(fd, pid_str, &delim, current_time);
+            close(fd);
+        }
+
+        if(difference <= 0)
+            break;
+        else if(difference == quarter)
+            fork_handler(WARNING);
+    }
+    remove(LOCKFILE); 
+    fork_handler(END);
+    return(0);
+}
+
+void fork_handler(ForkCase_t x){
+    int rc;
+    char *buffer;
+    
+    switch(x){
+        case START:
+            buffer = "Time Starting";
+            break;
+        case WARNING: 
+            buffer = "Ending Soon";
+            break;
+        case TERMINATE:
+            buffer = "Terminated";
+            break;
+        case END:
+            buffer = "Times Up";
+            break;
+        default:
+            break;
+    }
+
+    if((rc = fork()) < 0){
+        fork_failed();
+    }
+    else if(rc == 0){
+        char *notify[] = {"herbe", TITLE, " ", buffer, NULL};
+        execvp(notify[0],notify);
+    }
+}
+
+void itoa(int nbr, char *arr){
+    int i = 0;
+
+    do{
+        arr[i++] = (nbr % 10) + '0';
+    }while((nbr /= 10) != 0);
+
+    arr[i] = '\0';
+    reverse(arr);
+}
+
+void timetoa(int time_left, char *buffer){
+    int i = 0, extra_zero = 0;
+    int m = time_left / 60;
+    int s = time_left % 60;
+
+    extra_zero = (s < 10 ? 1 : 0);
+
+    do{
+        buffer[i++] = (s % 10) + '0';
+    }while((s /= 10) != 0);
+
+    if(extra_zero)
+        buffer[i++] = '0';
+    buffer[i++] = ':';
+
+    do{
+        buffer[i++] = (m % 10) + '0';
+    }while((m /= 10) != 0);
+
+    buffer[i] = '\0';
+    reverse(buffer);    
+}
+
+void reverse(char *b){
+    int i, j, c;
+
+    for(i = 0, j = strlen(b)-1; i < j; i++, j--){
+        c = b[j];
+        b[j] = b[i];
+        b[i] = c;
+    }
+}
+
+void write_to_lockfile(int fd, char *pid_str, char *delim, char *current_time){
+    write(fd, pid_str, sizeof(char) * strlen(pid_str));
+    write(fd, delim, sizeof(char));
+    write(fd, current_time, sizeof(char) * strlen(current_time));
+}
+
+void sig_handler(int sig){
+    switch(sig){
+        case SIGTERM:
+            remove(LOCKFILE); 
+            fork_handler(TERMINATE);
+            exit(0);
+        case SIGINT:
+            remove(LOCKFILE); 
+            fork_handler(TERMINATE);
+            exit(0);
+        default:
+            fprintf(stderr, "default case\n");
+            exit(1);
+    }
+}
+
+void fork_failed(void){
+    fprintf(stderr, "fork failed\n");
+    exit(1);
+}
